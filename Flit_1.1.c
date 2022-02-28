93898b82ac6cc3bfb1e4e3aaedf57413c8574648
Aleksyak <96631769+Aleksyak@users.noreply.github.com>
2022-02-28 19:44:24 +0300

    Add files via upload

---
+++ Flit_1.1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+int a[10];
+int b[10];
+int  i,k,f,h;
+  
+printf("Ввод первого массива \n");
+  
+for (i=0; i<3; i++)
+{
+   scanf("%d", &a[i]);
+}
+  
+printf("Ввод второго массива \n");
+for (k=0; k<3; k++)
+{
+   scanf("%d", &b[k]);
+}
+
+printf("Ввод номера элемента для удаления \n");
+scanf("%d", &f);
+a[f]=" ";
+
+printf("Ввод элемента на замену \n");
+scanf("%d", &h);
+a[f]=h;
+
+printf("Вывод первого массива \n");
+for (i=0; i<3; i++)
+{
+  printf("%d\n", a[i]);
+}
+
+printf("Вывод второго массива \n");
+for (k=0; k<3; k++)
+{
+ printf("%d\n", b[k]);
+}
+}
